b508bd02b2a4d18e1fd76029a5361e3433f46d2a
liyijun <496701974@qq.com>
2022-10-12 16:16:32 +0800

    力扣提交

---
+++ src/main/java/面试题/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+package 面试题;
+
+/**
+ *  力扣14：最长公共前缀
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) {
+            return "";
+        }
+        String s = strs[0];
+
+        for (String str : strs) {
+            while (!str.startsWith(s)) {
+                if (s.length() == 0) {
+                    return "";
+                }
+                s = s.substring(0, s.length() - 1);
+            }
+        }
+        return s;
+    }
+}
---
+++ src/main/java/面试题/RomeRevert.java
@@ -0,0 +1,53 @@
+package 面试题;
+
+/**
+ *  力扣13：罗马数字转整数
+ */
+public class RomeRevert {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int romanToInt(String s) {
+        String str = getRomeStr(s);
+
+        int res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            res += getIntVal(str.charAt(i));
+        }
+        return res;
+    }
+
+    public String getRomeStr(String str) {
+        str = str.replace("IV", "a");
+        str = str.replace("IX", "b");
+        str = str.replace("XL", "c");
+        str = str.replace("XC", "d");
+        str = str.replace("CD", "e");
+        str = str.replace("CM", "f");
+        return str;
+    }
+
+    public int getIntVal(char c) {
+        switch (c) {
+            case 'I' : return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            case 'a': return 4;
+            case 'b': return 9;
+            case 'c': return 40;
+            case 'd': return 90;
+            case 'e': return 400;
+            case 'f': return 900;
+        }
+
+        return 0;
+    }
+
+
+}
